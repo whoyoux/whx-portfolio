ec4fbd4b33f2534d1f1f6639ccfda4ceee63d327
whoyoux <48690153+whoyoux@users.noreply.github.com>
2023-02-28 11:49:09 +0100

    changed model

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -3,22 +3,27 @@ import React, { Suspense, useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useGLTF, OrbitControls, Stats } from "@react-three/drei";
+import { Mesh } from "three";
 
 const Showcase = () => {
-  const glassObj = useLoader(GLTFLoader, "/scene.glb");
+  const model = useLoader(GLTFLoader, "/model/scene.gltf");
   return (
-    <div className="w-full h-48 bg-red-900 flex justify-center items-center select-none">
+    <div className="w-full h-48 bg-red-900 flex justify-center items-center select-none relative">
       {/* 3d showcase */}
       <Canvas>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <Suspense fallback={null}>
-          <mesh rotation={[-90, 0, 0]} position={[0, 0, 0]}>
-            <primitive object={glassObj.scene} />
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <mesh position={[0, -2, 0]} scale={[4, 4, 4]}>
+            <primitive object={model.scene} />
+            <meshNormalMaterial attach="material" />
           </mesh>
         </Suspense>
-        {/* <OrbitControls target={[0, 1, 0]} /> */}
+        <OrbitControls target={[0, 0, 0]} />
+        {/* <Stats  /> */}
       </Canvas>
+      <span className="absolute bottom-0">needs to change model</span>
     </div>
   );
 };
